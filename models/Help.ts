134e942d6cbf57a8003b331a5cd6049f47aca82c
Jake <jakemillerdev@gmail.com>
2021-03-06 08:35:15 -0800

    Help updated.

--- models/Help.ts
+++ models/Help.ts
@@ -9,8 +9,16 @@ export default class HelpObject {
   
           },
           {
-            name: "cryptobot *coin_symbol || coin_name*",
-            value: "List information about single coin. (Use like: cryptobot btc)"
+            name: "cryptobot list 50-75",
+            value: "Query cryptocurrenies ranked by market cap (max 25)"
+          },
+          {
+            name: "cryptobot symbol/name",
+            value: "List information about single coin. (i.e. cryptobot btc)"
+          },
+          {
+            name: "cryptobot compare c1 c2 c3 c4",
+            value: "Compare recent market performance of several cryptocurrencies."
           }
         ]
       }
